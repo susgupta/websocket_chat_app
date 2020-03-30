8ee6beadd62553a284f8ced328a16520a4ce61ec
susgupta <guptask.7@gmail.com>
2020-03-30 18:27:34 -0400

    Flipped port resolution on server.js
    Flipped port resolution on server.js

--- server.js
+++ server.js
@@ -68,5 +68,5 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
